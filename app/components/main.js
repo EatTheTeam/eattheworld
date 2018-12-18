b216c4449af33549fc00545db6b6ce7a25bd11ee
EatTheMember <4123@htl.rennweg.at>
2018-12-18 12:52:47 +0100

    Fix Sidenav

--- app/components/main.js
+++ app/components/main.js
@@ -18,7 +18,17 @@ app.controller('MainController', function ($state, $mdSidenav, $mdMedia, $scope)
         modules: [{
             name: "General",
             state: "japan-general"
-        }]
+        },{
+            name: "1",
+            state: "japan-uhtgrd"
+        },{
+            name: "2",
+            state: "japan-tgrefwd"
+        },{
+            name: "3",
+            state: "japan-ggdrgrdl"
+        },
+        ]
     }, {
         name: "Ethiopia",
         state: "ethiopia",
@@ -34,11 +44,19 @@ app.controller('MainController', function ($state, $mdSidenav, $mdMedia, $scope)
         modules: []
     }];
 
+    for (const country of this.navData) {
+        if ([country, ...country.modules].some(e => e.state === $state.current.name)) {
+            country.expand = true;
+            break;
+        }
+    }
+
     this.menuLockedOpen = () => false; //$mdMedia('gt-md') && $state.current.name !== 'home';
 
-    this.checkState = (name) => $state.current.name === name;
+    this.isState = name => $state.current.name === name;
 
-    this.isJapan = () => this.checkState('japan') || this.checkState('japan-general');
-
-    this.isEthiopia = () => this.checkState('ethiopia') || this.checkState('ethiopia-general');
+    this.isCountryState = country =>
+        [country, ...country.modules].some(e => this.isState(e.state));
+    this.isEthiopia = () => this.isCountryState(this.navData.find(e => e.name === 'Ethiopia'));
+    this.isJapan = () => this.isCountryState(this.navData.find(e => e.name === 'Japan'));
 });
