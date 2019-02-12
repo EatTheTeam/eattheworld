c9f1c82e3db20983eb175cb7070daf81410e40fa
Thomas <4208@htl.rennweg.at>
2019-02-12 11:12:12 +0100

    Ethiopia Quiz State hinzugefügt

---
+++ app/components/ethiopia/ethiopia-quiz.js
@@ -0,0 +1,9 @@
+'use strict';
+
+app.component('ethiopiaQuiz', {
+    templateUrl: 'components/ethiopia/ethiopia-quiz.html',
+    controller: 'EthiopiaQuizController'
+});
+
+app.controller('EthiopiaQuizController', function () {
+});
