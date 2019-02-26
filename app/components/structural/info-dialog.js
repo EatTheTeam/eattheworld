88522f8b6cad038f2364babb4e9a01ff528ad56b
OpenByte <development.openbyte@gmail.com>
2019-02-26 16:18:12 +0100

    Fixed info dialog

--- app/components/structural/info-dialog.js
+++ app/components/structural/info-dialog.js
@@ -13,13 +13,20 @@ app.controller('InfoDialogController', function ($mdDialog) {
 
     this.show = e =>
         $mdDialog.show(
-            $mdDialog.alert()
-                .parent(angular.element(document.querySelector('#popupContainer')))
-                .clickOutsideToClose(true)
-                .title(this.titleText || 'Info')
-                .textContent(this.text)
-                .ok('Got it!')
-                .targetEvent(e)
+            $mdDialog.alert({
+                controller: function($element) {
+                    const dialog = $element[0];
+                    dialog.classList.add('info-dialog');
+                    const closeButton = dialog.getElementsByClassName('md-button')[0];
+                    closeButton.addEventListener('click', () => $mdDialog.hide(), false);
+                }
+            })
+            .parent(angular.element(document.querySelector('#popupContainer')))
+            .clickOutsideToClose(true)
+            .title(this.titleText || 'Info')
+            .textContent(this.text)
+            .ok('Close')
+            .targetEvent(e)
         );
 
 });
