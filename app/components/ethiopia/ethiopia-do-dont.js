fa84aff073b11a4ecd261ae1bd01dc449a902fec
elisabethmelaku <4138@htl.rennweg.at>
2019-02-26 11:01:24 +0100

    alle module erzeugt

---
+++ app/components/ethiopia/ethiopia-do-dont.js
@@ -0,0 +1,9 @@
+'use strict';
+
+app.component('ethiopiaDoDont', {
+    templateUrl: 'components/ethiopia/ethiopia-do-dont.html',
+    controller: 'EthiopiaDoDontController'
+});
+
+app.controller('EthiopiaDoDontController', function () {
+});
---
+++ app/components/ethiopia/ethiopia-rituals.js
@@ -0,0 +1,9 @@
+'use strict';
+
+app.component('ethiopiaRituals', {
+    templateUrl: 'components/ethiopia/ethiopia-rituals.html',
+    controller: 'EthiopiaRitualsController'
+});
+
+app.controller('EthiopiaRitualsController', function () {
+});
