e58b4b4a7025569dca2dea9b3f4e5bc0f1ed520a
OpenByte <development.openbyte@gmail.com>
2019-03-19 15:31:47 +0100

    Upgrade info dialog

--- app/components/structural/info-dialog.js
+++ app/components/structural/info-dialog.js
@@ -5,28 +5,43 @@ app.component('infoDialog', {
     controller: 'InfoDialogController',
     bindings: {
         titleText: '@?',
-        text: '@',
-    }
+        text: '@?',
+    },
+    transclude: true
 });
 
-app.controller('InfoDialogController', function ($mdDialog) {
+app.controller('InfoDialogController', function ($mdDialog, $transclude) {
+    this.$onInit = () => {
+        if ((this.text || '').trim() === '')
+            $transclude(elements => {
+                this.dialogContent = Array.from(elements).reduce((a, e) => {
+                    if (e.nodeType === Node.TEXT_NODE) // text
+                        return a + e.textContent;
+                    if (e.nodeType === Node.ELEMENT_NODE)
+                        return a + e.outerHTML;
+                    return a;
+                }, '');
+            });
+    };
 
-    this.show = e =>
-        $mdDialog.show(
-            $mdDialog.alert({
-                controller: function($element) {
-                    const dialog = $element[0];
-                    dialog.classList.add('info-dialog');
-                    const closeButton = dialog.getElementsByClassName('md-button')[0];
-                    closeButton.addEventListener('click', () => $mdDialog.hide(), false);
-                }
-            })
+    this.show = e => {
+        let dialog = $mdDialog.alert({
+            controller: function ($element) {
+                const dialog = $element[0];
+                dialog.classList.add('info-dialog');
+                const closeButton = dialog.getElementsByClassName('md-button')[0];
+                closeButton.addEventListener('click', () => $mdDialog.hide(), false);
+            }
+        })
             .parent(angular.element(document.querySelector('#popupContainer')))
             .clickOutsideToClose(true)
             .title(this.titleText || 'Info')
-            .textContent(this.text)
             .ok('Close')
-            .targetEvent(e)
-        );
+            .targetEvent(e);
+        if (this.dialogContent)
+            dialog = dialog.htmlContent(this.dialogContent);
+        else dialog = dialog.textContent(this.text);
+        $mdDialog.show(dialog);
+    };
 
 });
