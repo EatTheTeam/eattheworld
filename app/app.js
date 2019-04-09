a9239318cead0b56828e2189fedebe720c4245a2
OpenByte <development.openbyte@gmail.com>
2019-04-09 10:21:46 +0200

    Deactivate Cache Killer

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@
 const app = angular.module("EatTheWorld", ["ngMaterial", "ngSanitize", "ui.router", "smoothScroll" /*"ngRoute", "ui.router" /*, "ngResource", "ngMessages", "ngLocale", "ngAnimate", , "ngStorage"*/]);
 
 app.config(['$httpProvider', function ($httpProvider) {
-    //initialize get if not there
+    /*//initialize get if not there
     if (!$httpProvider.defaults.headers.get) {
         $httpProvider.defaults.headers.get = {};
     }
@@ -15,7 +15,7 @@ app.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
     // extra
     $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
-    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';*/
 }]);
 
 app.config($mdThemingProvider => {
